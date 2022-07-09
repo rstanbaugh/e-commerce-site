9bf126836dad64ba6077475120520658336b4cb1
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-09 14:28:47 -0400

    DELETE product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -145,8 +145,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   console.log('=========== DELETE PRODUCT ===========');
-
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      rs.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
