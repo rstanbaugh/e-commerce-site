f3f6603a9053b5946d309ace4a9a15d91b27d9ab
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-09 13:17:01 -0400

    database config settings

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.listen(PORT, () => {console.log(`App listening on port ${PORT}!`);
+});
+
+// turn on connection to db and server
+// set force: true so tables re-creat/ false to turn off
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
